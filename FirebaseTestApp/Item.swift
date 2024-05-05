d55818e31a6c63980cc0b056d7a9be60c861ae64
t4lz <t4lz.git@gmail.com>
2024-05-05 20:53:13 +0200

    Xcode-generated code for ios app
    I haven't made any changes yet, this was generated by Xcode
    with the ios app template.

---
+++ FirebaseTestApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FirebaseTestApp
+//
+//  Created by Tal on 05.05.24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
